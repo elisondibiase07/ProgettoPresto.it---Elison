6fd93734277360e0b15b9d2bbff085e1764429ec
Elison <you@example.comelisondibiase@gmail.com>
2023-03-10 19:30:36 +0100

    numeri incrementali terminati

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let navbar = document.querySelector('#navbar');
+let number1 = document.querySelector('#number1');
+let number2 = document.querySelector('#number2');
 
 window.addEventListener('scroll', ()=>{
     let scrolled = window.scrollY ;
@@ -12,3 +14,48 @@ window.addEventListener('scroll', ()=>{
         
     }
 });
+
+let confirm = true;
+
+function createInterval(n , el , time){
+
+    let counter = 0
+    let interval = setInterval( ()=> {
+    
+    if(counter < n){
+        counter++
+        el.innerHTML = counter;
+    } else{
+
+        clearInterval(interval)
+       
+    }
+    
+    }, time);
+
+    // setTimeout(()=> {
+    //     confirm = true ;
+
+    // }, 8000);
+
+}
+
+
+
+
+let observer = new IntersectionObserver( (entries)=>{
+
+    entries.forEach((entry) => {
+
+        if(entry.isIntersecting && confirm){
+            createInterval(1000 , number1 , 100);
+            createInterval(100 , number2 , 1000);
+            // confirm = false;
+        }
+        
+    });
+
+
+} );
+
+observer.observe(number1);
