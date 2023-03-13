ba28cb227cf82428102ccea0fec5adeb4b46db71
Elison <you@example.comelisondibiase@gmail.com>
2023-03-13 17:35:43 +0100

    recensioni

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ let confirm = true;
 
 function createInterval(n , el , time){
 
-    let counter = 0
+    let counter = 0 ;
     let interval = setInterval( ()=> {
     
     if(counter < n){
@@ -28,16 +28,16 @@ function createInterval(n , el , time){
         el.innerHTML = counter;
     } else{
 
-        clearInterval(interval)
-       
+        console.log('Adesso mi fermo');
+        clearInterval(interval) ;
     }
     
     }, time);
 
-    // setTimeout(()=> {
-    //     confirm = true ;
+    setTimeout(()=> {
+        confirm = true ;
 
-    // }, 8000);
+    }, 000);
 
 }
 
@@ -49,9 +49,9 @@ let observer = new IntersectionObserver( (entries)=>{
     entries.forEach((entry) => {
 
         if(entry.isIntersecting && confirm){
-            createInterval(1000 , number1 , 100);
-            createInterval(100 , number2 , 1000);
-            // confirm = false;
+            createInterval(30 , number1 , 40);
+            createInterval(50 , number2 , 60);
+            confirm = false;
         }
         
     });
@@ -77,7 +77,96 @@ const swiper = new Swiper('.swiper', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  
+
    
   });
 
+
+
+
+
+  let swiperwrapperSecond = document.querySelector('.swiper-wrapperSecond');
+
+
+let reviews = [
+    {user : `Elison `, description : `Bellissimo ` , rank : 5},
+    {user : `Debora `, description : `Non male ` , rank : 3},
+    {user : `Serena `, description : `Pessimo ` , rank : 1},
+]
+
+
+reviews.forEach((recensione)=> {
+    let div = document.createElement('div')
+    div.classList.add('swiper-slide-second')
+    div.innerHTML= `
+    <div class="card-review">
+    <p class="text-center">${recensione.description}</p>
+        <p class="text-center">${recensione.user}</p>
+    <div class=" div-icon d-flex justify-content-center star">
+        
+    </div>
+</div>
+    
+    ` ;
+    swiperwrapperSecond.appendChild(div);
+
+}) ;
+
+
+
+
+
+let stars = document.querySelectorAll('.star');
+
+stars.forEach((star , index)=> {
+
+for(let i = 1 ; i <= reviews[index].rank ; i++){
+
+let icon = document.createElement('i') ;
+icon.classList.add('fa-solid`, `fa-star');
+star.appendChild(icon);
+
+}
+
+let difference = 5 - reviews[index].rank ;
+
+for(let i = 1 ; i <= difference ; i++){
+
+    let icon = document.createElement('i') ;
+
+    icon.classList.add('fa-regular`, `fa-star');
+    star.appendChild(icon);
+    
+    }
+
+});
+
+
+
+// Swiper2
+
+const swiper2 = new Swiper('.swiperSecond', {
+    // Optional parameters
+
+    loop: true,
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+   
+    },
+    
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+   
+  });
